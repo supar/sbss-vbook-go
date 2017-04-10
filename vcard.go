d982ffc5787f6b2e838fa862a4ae4ac0519950f9
Paul Rezunenko <rezunenko@lanbilling.ru>
2017-04-10 17:17:04 +0300

    Генератор vcard из структуры

---
+++ vcard.go
@@ -0,0 +1,337 @@
+package main
+
+import (
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+const (
+	VCardTagName = "vcard"
+)
+
+// Stuct tags
+// `vcard:"fieldNamme,inlineitems(;),inline(;),omitname,separator(=),wrapvcard,version(3.0)"`
+// Exportaed field name is required, You may live empty to take struct original field name
+// Field name wil be upper case always
+// inlineitem - for the none primitive fields will  join data in line string
+// inline - join all field data inline string
+// omitaname - do not append field name and write value only
+// separator - change default label separator
+//
+//     struct{
+//         Tel struct {
+//             Type string    `vcard:",separator(=)"`
+//             Value string   `vcard:",omitname"`
+//         }                  `vcard:",iteminline(:),separator(;)"`
+//     }
+type fieldStruct struct {
+	// Field name
+	name string
+	// Join all struct data in line string
+	inline bool
+	glue   string
+	// If struct field is slice, then
+	// slice items can be joined in line string
+	iteminline bool
+	itemglue   string
+	// Label separator
+	separator string
+	// Skip fields with empty data
+	omitempty bool
+	// Skip field label, just write value
+	omitname bool
+	// Skip field
+	skip bool
+	// Write vcard virsion if wrap option is on
+	version    bool
+	versionnum string
+	// Wrap data with VCARD tokens
+	wrapvcard bool
+}
+
+func marshal(v interface{}) []byte {
+	return element(reflect.ValueOf(v), nil)
+}
+
+func element(v reflect.Value, opts *fieldStruct) (data []byte) {
+	if opts != nil {
+		if opts.skip {
+			return
+		}
+	}
+
+	switch v.Kind() {
+	case reflect.Ptr, reflect.Interface:
+		if !v.IsNil() {
+			data = element(v.Elem(), opts)
+		}
+
+	case reflect.Slice, reflect.Array:
+		data = walkSlice(v, opts)
+
+	case reflect.Struct:
+		data = walkStruct(v, opts)
+
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64,
+		reflect.String:
+
+		data = primitive(v, opts)
+	}
+
+	return
+}
+
+func walkSlice(v reflect.Value, opts *fieldStruct) (data []byte) {
+	var (
+		length = v.Len()
+	)
+
+	for i := 0; i < length; i++ {
+		item := element(v.Index(i), opts)
+
+		if len(item) > 0 {
+			if opts != nil {
+				if !opts.inline && opts.iteminline && !opts.omitname {
+					item = append([]byte(opts.name+opts.separator), item...)
+				}
+			}
+
+			data = append(data, item...)
+
+			if i < (length - 1) {
+				if opts != nil && opts.inline {
+					data = append(data, []byte(opts.glue)...)
+				} else {
+					data = append(data, []byte("\n")...)
+				}
+			}
+		}
+	}
+
+	return
+}
+
+func walkStruct(v reflect.Value, opts *fieldStruct) (data []byte) {
+	var (
+		field     reflect.Value
+		fieldOpts fieldStruct
+
+		itemType = v.Type()
+		length   = itemType.NumField()
+	)
+
+	for i := 0; i < length; i++ {
+		field = v.Field(i)
+
+		fieldOpts = fieldOptions(itemType.Field(i))
+
+		item := element(field, &fieldOpts)
+
+		if !fieldOpts.omitname {
+			switch field.Kind() {
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+				reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+				reflect.Float32, reflect.Float64,
+				reflect.String:
+
+				item = append([]byte(fieldOpts.name+fieldOpts.separator), item...)
+
+			default:
+				if fieldOpts.inline {
+					item = append([]byte(fieldOpts.name+fieldOpts.separator), item...)
+				}
+			}
+		}
+
+		if len(item) > 0 {
+			data = append(data, item...)
+
+			if i < (length - 1) {
+				if opts != nil && opts.iteminline {
+					data = append(data, []byte(opts.itemglue)...)
+				} else {
+					data = append(data, []byte("\n")...)
+				}
+			}
+		}
+	}
+
+	return wrapVcard(data, opts)
+}
+
+func wrapVcard(data []byte, opts *fieldStruct) (vr []byte) {
+	if opts == nil || !opts.wrapvcard || len(data) == 0 {
+		return data
+	}
+
+	vc := [][]byte{
+		[]byte("BEGIN:VCARD"),
+		[]byte("VERSION:" + opts.versionnum),
+		data,
+		[]byte("END:VCARD"),
+	}
+
+	for idx, val := range vc {
+		if idx == 1 && !opts.version {
+			continue
+		}
+
+		vr = append(vr, val...)
+
+		if opts.inline {
+			vr = append(vr, []byte("\\n")...)
+		} else {
+			vr = append(vr, []byte("\n")...)
+		}
+	}
+
+	return
+
+}
+
+func primitive(v reflect.Value, opts *fieldStruct) (data []byte) {
+	if opts == nil {
+		panic("field options required")
+	}
+
+	if opts.skip {
+		return
+	}
+
+	switch v.Kind() {
+	case reflect.String:
+		if v.String() == "" && opts.omitempty {
+			return
+		}
+
+		return []byte(v.String())
+
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return []byte(strconv.FormatInt(v.Int(), 10))
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return []byte(strconv.FormatUint(v.Uint(), 10))
+
+	case reflect.Float32:
+		return []byte(strconv.FormatFloat(v.Float(), 'f', -1, 32))
+
+	case reflect.Float64:
+		return []byte(strconv.FormatFloat(v.Float(), 'f', -1, 64))
+	}
+
+	return
+}
+
+// Get field properties
+func fieldOptions(f reflect.StructField) (fs fieldStruct) {
+	var (
+		parts []string
+
+		tag = f.Tag.Get(VCardTagName)
+	)
+
+	if tag == "-" {
+		fs.skip = true
+		return
+	}
+
+	parts = strings.Split(tag, ",")
+	fs.name = parts[0]
+
+	if fs.name == "" {
+		fs.name = f.Name
+	}
+
+	fs.name = strings.ToUpper(fs.name)
+	fs.glue = ";"
+	fs.itemglue = ";"
+	fs.separator = ":"
+	fs.versionnum = "3.0"
+
+	for _, s := range parts[1:] {
+		if s == "omitempty" {
+			fs.omitempty = true
+
+			continue
+		}
+
+		if s == "wrapvcard" {
+			fs.wrapvcard = true
+
+			continue
+		}
+
+		if s == "omitname" {
+			fs.omitname = true
+
+			continue
+		}
+
+		if strings.HasPrefix(s, "inline") {
+			s = strings.Trim(
+				strings.TrimPrefix(s, "inline"),
+				" ()",
+			)
+
+			fs.inline = true
+
+			if s != "" {
+				fs.glue = s
+			}
+
+			continue
+		}
+
+		if strings.HasPrefix(s, "iteminline") {
+			s = strings.Trim(
+				strings.TrimPrefix(s, "iteminline"),
+				" ()",
+			)
+
+			fs.iteminline = true
+
+			if s != "" {
+				fs.itemglue = s
+			}
+
+			continue
+		}
+
+		if strings.HasPrefix(s, "separator") {
+			s = strings.Trim(
+				strings.TrimPrefix(s, "separator"),
+				" ()",
+			)
+
+			if s != "" {
+				fs.separator = s
+			}
+
+			continue
+		}
+
+		if strings.HasPrefix(s, "version") {
+			s = strings.Trim(
+				strings.TrimPrefix(s, "version"),
+				" ()",
+			)
+
+			fs.version = true
+
+			if s != "" {
+				fs.versionnum = s
+			}
+
+			continue
+		}
+	}
+
+	if fs.inline && !fs.iteminline {
+		fs.iteminline = true
+	}
+
+	return
+}
---
+++ vcard_test.go
@@ -0,0 +1,254 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+type vCard_Contact_Test struct {
+	Name string `vcard:"fn"`
+}
+
+type vCard_Phone_Test struct {
+	Type  string `vcard:",separator(=)"`
+	Value string `vcard:",omitname"`
+}
+
+func Test_EncodeSimpleSingleStruct(t *testing.T) {
+	var (
+		vc = struct {
+			Name   string             `vcard:"fn"`
+			Login  string             `vcard:"nickname"`
+			Slice  []string           `vcard:"n,inline"`
+			Phones []vCard_Phone_Test `vcard:"tel,iteminline(:),separator(;)"`
+		}{
+			Name:  "Ivan Ivanovich Ivanov",
+			Login: "ivanovlogin",
+			Slice: []string{"Ivan", "Ivanov", "Ivanovich"},
+			Phones: []vCard_Phone_Test{
+				vCard_Phone_Test{
+					Type:  "cell",
+					Value: "+123 00 0000000",
+				},
+				vCard_Phone_Test{
+					Type:  "work",
+					Value: "+123 00 1200000",
+				},
+				vCard_Phone_Test{
+					Type:  "home",
+					Value: "+123 00 1230000",
+				},
+			},
+		}
+
+		mock = `FN:Ivan Ivanovich Ivanov
+NICKNAME:ivanovlogin
+N:Ivan;Ivanov;Ivanovich
+TEL;TYPE=cell:+123 00 0000000
+TEL;TYPE=work:+123 00 1200000
+TEL;TYPE=home:+123 00 1230000`
+	)
+
+	if d := marshal(vc); string(d) != mock {
+		t.Error("Unexpected result")
+		t.Logf("%s", d)
+		t.Log(mock)
+	}
+}
+
+func Test_CardsDataArray_CheckNilOptions(t *testing.T) {
+	var (
+		vc = []vCard_Contact_Test{
+			vCard_Contact_Test{
+				Name: "Samuel J",
+			},
+			vCard_Contact_Test{
+				Name: "Noname C",
+			},
+		}
+
+		mock = `FN:Samuel J
+FN:Noname C`
+	)
+
+	if d := marshal(vc); string(d) != mock {
+		t.Error("Unexpected result")
+		t.Logf("%s", d)
+		t.Log(mock)
+	}
+}
+
+func Test_AddressBook(t *testing.T) {
+	var (
+		vc = struct {
+			contact []vCard_Contact_Test `vcard:",wrapvcard,version(3.1)"`
+			agent   vCard_Contact_Test   `vcard:"agent,wrapvcard,inline"`
+		}{
+			contact: []vCard_Contact_Test{
+				vCard_Contact_Test{
+					Name: "Some User A",
+				},
+				vCard_Contact_Test{
+					Name: "Some User B",
+				},
+			},
+			agent: vCard_Contact_Test{
+				Name: "Some coworker C",
+			},
+		}
+
+		mock = `BEGIN:VCARD
+VERSION:3.1
+FN:Some User A
+END:VCARD
+
+BEGIN:VCARD
+VERSION:3.1
+FN:Some User B
+END:VCARD
+
+AGENT:BEGIN:VCARD\nFN:Some coworker C\nEND:VCARD\n`
+	)
+
+	if d := marshal(vc); string(d) != mock {
+		t.Error("Unexpected result")
+		t.Logf("%s", d)
+		t.Log(mock)
+	}
+}
+
+func Test_WrapDataWithVcardTokens(t *testing.T) {
+	var (
+		data = []byte("FN:Some Name")
+
+		mock = []string{
+			`BEGIN:VCARD
+FN:Some Name
+END:VCARD
+`,
+			`BEGIN:VCARD
+VERSION:3.0
+FN:Some Name
+END:VCARD
+`,
+			`BEGIN:VCARD\nFN:Some Name\nEND:VCARD\n`,
+			`BEGIN:VCARD\nVERSION:3.0\nFN:Some Name\nEND:VCARD\n`,
+		}
+	)
+
+	for i, v := range mock {
+		opts := &fieldStruct{wrapvcard: true}
+
+		switch i {
+		case 1:
+			opts.version = true
+			opts.versionnum = "3.0"
+
+		case 2:
+			opts.inline = true
+
+		case 3:
+			opts.inline = true
+			opts.version = true
+			opts.versionnum = "3.0"
+		}
+
+		r := wrapVcard(data, opts)
+
+		if s := string(r); s != v {
+			t.Error("Unexpected result")
+			t.Log(s)
+			t.Log(v)
+		}
+	}
+}
+
+func Test_FieldOptionsValid(t *testing.T) {
+	var (
+		m fieldStruct
+
+		f = reflect.TypeOf(struct {
+			Name      string `vcard:"FN"`
+			Nickname  string `vcard:",inline"`
+			Skipfield string `vcard:"-"`
+			Omitfield string `vcard:"omit,omitempty"`
+			Glue      string `vcard:",inline(\n)"`
+			NoName    string `vcard:",omitname"`
+		}{
+			Name:      "Name",
+			Nickname:  "Nickname",
+			Skipfield: "Skip",
+			NoName:    "Skip field label",
+		})
+	)
+
+	for i := 0; i < f.NumField(); i++ {
+		ft := f.Field(i)
+		o := fieldOptions(ft)
+
+		switch i {
+		case 0:
+			m = fieldStruct{
+				name:       "FN",
+				glue:       ";",
+				itemglue:   ";",
+				separator:  ":",
+				versionnum: "3.0",
+			}
+
+		case 1:
+			m = fieldStruct{
+				name:       "NICKNAME",
+				inline:     true,
+				iteminline: true,
+				glue:       ";",
+				itemglue:   ";",
+				separator:  ":",
+				versionnum: "3.0",
+			}
+
+		case 2:
+			m = fieldStruct{
+				skip: true,
+			}
+
+		case 3:
+			m = fieldStruct{
+				name:       "OMIT",
+				omitempty:  true,
+				glue:       ";",
+				itemglue:   ";",
+				separator:  ":",
+				versionnum: "3.0",
+			}
+
+		case 4:
+			m = fieldStruct{
+				name:       "GLUE",
+				inline:     true,
+				iteminline: true,
+				glue:       "\n",
+				itemglue:   ";",
+				separator:  ":",
+				versionnum: "3.0",
+			}
+
+		case 5:
+			m = fieldStruct{
+				name:       "NONAME",
+				omitname:   true,
+				glue:       ";",
+				itemglue:   ";",
+				separator:  ":",
+				versionnum: "3.0",
+			}
+		}
+
+		if !reflect.DeepEqual(
+			m,
+			o,
+		) {
+			t.Errorf("Invalid field options: %v (%v)", o, m)
+		}
+	}
+}
